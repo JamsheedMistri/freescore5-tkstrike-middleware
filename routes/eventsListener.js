6ab3b1110a489acf34819c77667e5ea262978e3f
Jamsheed Mistri <jmistri@berkeley.edu>
2023-01-08 23:47:14 -0800

    Event listener endpoints implemented

--- routes/eventsListener.js
+++ routes/eventsListener.js
@@ -6,4 +6,22 @@ router.get('/ping', (req, res) => {
 	res.send({ status: 'ok' });
 });
 
+router.post('/new-match-event', (req, res) => {
+	console.log('New Match Event:');
+	console.log(req.body);
+	res.send({ status: 'ok' });
+});
+
+router.post('/new-match-configured', (req, res) => {
+	console.log('New Match Configured:');
+	console.log(req.body);
+	res.send({ status: 'ok' });
+});
+
+router.post('/match-result', (req, res) => {
+	console.log('Match Result:');
+	console.log(req.body);
+	res.send({ status: 'ok' });
+});
+
 module.exports = router;
