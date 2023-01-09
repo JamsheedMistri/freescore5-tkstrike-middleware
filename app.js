efa8c7ac20f2d9a4ddb86dea6561c786c29590b7
Jamsheed Mistri <jmistri@berkeley.edu>
2023-01-08 23:24:33 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const helmet = require('helmet');
+
+// Routers
+const venueManagementRouter = require('./routes/venueManagement');
+const eventsListenerRouter = require('./routes/eventsListener');
+
+// Middleware
+const errorHandler = require('./middleware/errorHandler');
+
+// Setup
+const app = express();
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Linkers
+app.use('/venue-management', venueManagementRouter);
+app.use('/events-listener', eventsListenerRouter);
+
+// Unhandled errors
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+	console.log(`freescore5-tkstrike-middleware started on port ${port}.`)
+);
